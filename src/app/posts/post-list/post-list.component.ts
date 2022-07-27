34a2647630a2ebf179906a34aa7df05ef774227d
aytgn <aytgnozgn@gmail.com>
2022-07-27 11:45:31 +0300

    first commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {
+  posts = [
+    {
+      title: 'First Post',
+      content: "First post's content",
+    },
+    {
+      title: 'Second Post',
+      content: "Second post's content",
+    },
+    {
+      title: 'Third Post',
+      content: "Third post's content",
+    },
+  ];
+}
