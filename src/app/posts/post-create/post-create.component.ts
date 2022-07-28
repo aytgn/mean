12bfd6f9de3ce3323f7f508ab7ad74ea167d6a57
aytgn <aytgnozgn@gmail.com>
2022-07-28 17:15:00 +0300

    form-morm

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from '../post.model';
 
 @Component({
@@ -7,13 +8,17 @@ import { Post } from '../post.model';
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredContent = '';
-  enteredTitle = '';
   newPost: Post = { title: '', content: '' };
 
   @Output() postCreated = new EventEmitter<Post>();
-  onPostSubmit() {
-    this.newPost = { title: this.enteredTitle, content: this.enteredContent };
-    this.postCreated.emit(this.newPost);
+  onAddPost(postForm: NgForm) {
+    if (postForm.invalid) return;
+    //modify newPost
+    this.newPost.title = postForm.value.title;
+    this.newPost.content = postForm.value.content;
+    //emit current copy of newPost obj!
+    this.postCreated.emit({ ...this.newPost });
+    //reset form
+    postForm.resetForm();
   }
 }
