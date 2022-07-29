ec014bad829c187d3396ff5e09bedb7daa3c0a57
aytgn <aytgnozgn@gmail.com>
2022-07-29 14:59:40 +0300

    okkkk

--- bakckend/app.js
+++ bakckend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -11,6 +13,13 @@ app.use((req, res, next) => {
     "Access-Controls-Allow-Methods",
     "GET, POST, PATCH, DELETE, OPTIONS"
   );
+  next();
+});
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({ message: "Post Added" });
   next();
 });
 
