d4140c304ea0f6f5ef2dbc40fb798f14dd5cdafb
aytgn <aytgnozgn@gmail.com>
2022-07-30 14:12:41 +0300

    yapıyoruz birşeyler

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,49 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Subject } from 'rxjs';
+import { first, map, Observable, Subject } from 'rxjs';
 import { Post } from './post.model';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
-  private posts: Post[] = [];
-  private postsUpdated: Subject<Post[]> = new Subject<Post[]>();
-
   constructor(private http: HttpClient) {}
 
-  getPosts() {
-    this.http
-      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+  getPostsOnce(): Observable<{ message: string; posts: Post[] }> {
+    return this.getPosts().pipe(first());
+  }
+  getPosts(): Observable<{ message: string; posts: Post[] }> {
+    return this.http
+      .get<{ message: string; posts: any[] }>('http://localhost:3000/api/posts')
       .pipe(
-        map((postData) => {
-          return postData.posts.map((post: any) => {
-            return {
-              title: post.title,
-              content: post.content,
-              id: post._id,
-            };
-          });
+        map((res) => {
+          const message = res.message;
+          const posts = res.posts.map((post) => ({
+            id: post._id,
+            title: post.title,
+            content: post.content,
+          }));
+          return { message, posts };
         })
-      )
-      .subscribe((postData) => {
-        //update the local posts
-        this.posts = postData.posts;
-        //carry to subscribers new local posts
-        this.postsUpdated.next([...this.posts]);
-      });
+      );
   }
-
-  getPostUpdateListener() {
-    return this.postsUpdated.asObservable();
-  }
-
   addPost(title: string, content: string) {
-    const post: Post = { id: 'newId', title, content };
-    this.http
-      .post('http://localhost:3000/api/posts', post)
-      .subscribe((responseData: any) => {
-        console.log(responseData.message);
-      });
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    const postToAdd = { title, content };
+    return this.http.post('http://localhost:3000/api/posts', postToAdd);
+  }
+  deletePost(postId: string) {
+    return this.http.delete('http://localhost:3000/api/posts/' + postId);
   }
 }
